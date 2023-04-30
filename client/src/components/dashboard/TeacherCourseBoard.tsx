3ebaac0c7bf225074b3124cb9c1e7546054fcb87
Hoang Duong <hoangduongphantri@gmail.com>
2023-04-30 18:37:10 +0300

    Create new Teacher course board

---
+++ client/src/components/dashboard/TeacherCourseBoard.tsx
@@ -0,0 +1,10 @@
+import { heading } from "@/styles/font"
+
+const TeacherCourseBoard = () => {
+  return (
+    <div>
+      <h2 style={heading.style}>Your own course</h2>
+    </div>
+  )
+}
+export default TeacherCourseBoard
